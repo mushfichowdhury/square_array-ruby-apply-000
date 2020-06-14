d9da3cf4d764dacef990e3244808efb300cece9b
Mushfi Chowdhury <chowdhurymushfi@gmail.com>
2020-06-14 21:29:49 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,9 @@
-def square_array(array)
-  # your code here
+numbers = [1,2,3]
+
+def square_array(numbers)
+  new_numbers = []
+  numbers.each do |number| 
+  new_numbers << number ** 2
+end
+return new_numbers
 end
